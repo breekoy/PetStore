4d13d0c5a9c065bb1e38de77be1168ef1d5a4998
coreDev Bree <coreDev Bree@DESKTOP-H9USVM6>
2022-06-05 12:29:00 +0800

    Add project files.

---
+++ JohanePetStore/MainForm.aspx
@@ -0,0 +1,112 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="JohanePetStore.MainForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+        .auto-style2 {
+            width: 100%;
+        }
+        .auto-style4 {
+            width: 249px;
+            text-align: right;
+        }
+        .auto-style5 {
+            width: 249px;
+            text-align: right;
+            height: 26px;
+        }
+        .auto-style6 {
+            height: 26px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <h1><strong>Johane Pet Store</strong></h1>
+        </div>
+        <p>
+            <asp:TextBox ID="txtPetID" runat="server" Visible="False"></asp:TextBox>
+        </p>
+        <table class="auto-style2" align="center">
+            <tr>
+                <td class="auto-style4">Pet Name</td>
+                <td>
+                    <asp:TextBox ID="txtPetName" runat="server" Width="553px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Pet Age</td>
+                <td>
+                    <asp:TextBox ID="txtPetAge" runat="server" Width="553px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style5">Pet Sex</td>
+                <td class="auto-style6">
+                    <asp:DropDownList ID="dropdownSex" runat="server">
+                        <asp:ListItem Selected="True">Male</asp:ListItem>
+                        <asp:ListItem>Female</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Pet Breed</td>
+                <td>
+                    <asp:TextBox ID="txtPetBreed" runat="server" Width="553px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Pet Status</td>
+                <td>
+                    <asp:TextBox ID="txtPetStatus" runat="server" Width="553px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Pet Price</td>
+                <td>
+                    <asp:TextBox ID="txtPetPrice" runat="server" Width="553px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">&nbsp;</td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style4">&nbsp;</td>
+                <td>
+            <asp:Button ID="btnAdd" runat="server" Height="36px" OnClick="btnAdd_Click" Text="Add" Width="161px" />
+            <asp:Button ID="btnUpdate" runat="server" Height="36px" OnClick="btnUpdate_Click" Text="Update" Width="161px" />
+            <asp:Button ID="btnDelete" runat="server" Height="36px" OnClick="btnDelete_Click" Text="Delete" Width="161px" />
+            <asp:Button ID="btnClear" runat="server" Height="36px" OnClick="btnClear_Click" Text="Clear" Width="161px" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">&nbsp;</td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style4">&nbsp;</td>
+                <td>
+                    <asp:GridView ID="gridviewPets" runat="server" AutoGenerateSelectButton="True" OnSelectedIndexChanged="gridviewPets_SelectedIndexChanged">
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ JohanePetStore/MainForm.aspx.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JohanePetStore
+{
+    public partial class MainForm : System.Web.UI.Page
+    {
+
+        //creates an sql connection instance using a connection string
+        SqlConnection connection = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=johanepetstore;User ID=sa;Password=*********");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ResetInputs();
+
+            if (!IsPostBack)
+            {
+                LoadRecord();
+            }
+        }
+
+        private void LoadRecord()
+        {
+            SqlCommand command = new SqlCommand("SELECT * from Pets", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            gridviewPets.DataSource = dataTable;
+            gridviewPets.DataBind();
+        }
+
+        private void ResetInputs()
+        {
+            //clear textbox
+            txtPetID.Text = "";
+            txtPetName.Text = "";
+            txtPetAge.Text = "";
+            dropdownSex.Text = "";
+            txtPetBreed.Text = "";
+            txtPetStatus.Text = "";
+            txtPetPrice.Text = "";
+
+            //clear the selected row from data table/grid
+            gridviewPets.SelectedIndex = -1;
+
+            //reset the buttons
+            btnAdd.Enabled = true;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            //stores values from textbox to variables
+            string petName = txtPetName.Text;
+            string petAge = txtPetAge.Text;
+            string petSex = dropdownSex.Text;
+            string petBreed = txtPetBreed.Text;
+            string petStatus = txtPetStatus.Text;
+            string petPrice = txtPetPrice.Text;
+
+            /*
+             * VALIDATIONS
+             * 
+             * Before we insert to database, we must first validate the values.
+             * This is to make sure that all of the values are correct.
+             * 
+             * Below are the following validations
+             * 
+             */
+
+            /*
+             * Validation #1: Pet name must have a value!
+             */
+            if (string.IsNullOrEmpty(petName)) 
+            {
+                //inserts and executes a javascript alert() to the page
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('You must fill up the pet name!')", true);
+
+                //stops the code execution
+                return;
+            }
+
+            /*
+             * Validation #2: Pet age must have a value!
+             */
+            if (string.IsNullOrEmpty(petAge))
+            {
+                //javascript alert
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('You must fill up the pet age!')", true);
+
+                //stops the code execution
+                return;
+            }
+
+            /*
+             * Validation #3: Pet age must be a number!
+             */
+            if (!double.TryParse(petAge, out double petAgeDoubleResult))
+            {
+                //javascript alert
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Pet age must be a number!')", true);
+
+                //stops the code execution
+                return;
+            }
+
+            /*
+             * Validation #4: Pet breed must have a value!
+             */
+            if (string.IsNullOrEmpty(petBreed))
+            {
+                //javascript alert
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('You must fill up the pet breed!')", true);
+
+                //stops the code execution
+                return;
+            }
+
+            /*
+             * Validation #5: Pet status must have a value!
+             */
+            if (string.IsNullOrEmpty(petStatus))
+            {
+                //javascript alert
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('You must fill up the pet status!')", true);
+
+                //stops the code execution
+                return;
+            }
+
+
+            /*
+             * Validation #6: Pet price must have a value!
+             */
+            if (string.IsNullOrEmpty(petPrice))
+            {
+                //javascript alert
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('You must fill up the pet price!')", true);
+
+                //stops the code execution
+                return;
+            }
+
+            /*
+             * Validation #7: Pet price must be a number!
+             */
+            if (!double.TryParse(petPrice, out double petPriceDoubleResult))
+            {
+                //javascript alert
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Pet price must be a number!')", true);
+
+                //stops the code execution
+                return;
+            }
+
+            /*
+             * ACTUAL INSERTION TO DATABASE
+             * 
+             * If all validations are met, we are now ready to insert the values to database.
+             * 
+             */
+
+            //opens the sql connection
+            connection.Open();
+
+            //creates an sql insert command
+            SqlCommand command = new SqlCommand(@"INSERT into Pets (Pet_Name, Pet_Age, Pet_Sex, Pet_Breed, Pet_Status, Pet_Price) 
+                VALUES ('" + petName + "', " + petAge + ", '" + petSex + "', '" + petBreed + "', '" + petStatus + "', '" + petPrice + "')", 
+             connection);
+            
+            //executes the command
+            command.ExecuteNonQuery();
+
+            //closes the sql connection
+            connection.Close();
+
+            //javascript success alert
+            ScriptManager.RegisterStartupScript(this, GetType(), "script", "alert('Record has been added to database!')", true);
+
+            //resets all inputs (textboxes, buttons, grid)
+            ResetInputs();
+
+            //refresh the data table
+            LoadRecord();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //stores values from textbox to variables
+            string petId = txtPetID.Text;
+            string petName = txtPetName.Text;
+            string petAge = txtPetAge.Text;
+            string petSex = dropdownSex.Text;
+            string petBreed = txtPetBreed.Text;
+            string petStatus = txtPetStatus.Text;
+            string petPrice = txtPetPrice.Text;
+
+            connection.Open();
+
+            //sql update command
+            SqlCommand command = new SqlCommand(@"UPDATE Pets SET " +
+                @"Pet_Name = '" + petName + "', " +
+                @"Pet_Age = '" + petAge + "', " + 
+                @"Pet_Sex = '" + petSex + "', " + 
+                @"Pet_Breed = '" + petBreed + "', " + 
+                @"Pet_Status = '" + petStatus + "', " + 
+                @"Pet_Price = '" + petPrice + "' " + 
+                @"WHERE Pet_ID = " + petId,
+             connection);
+
+            //executes the command
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
+            ScriptManager.RegisterStartupScript(this, GetType(), "script", "alert('Record successfully updated!')", true);
+
+
+            //resets all inputs (textboxes, buttons, grid)
+            ResetInputs();
+
+            //refresh the data table/grid
+            LoadRecord();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            string petId = txtPetID.Text;
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(@"DELETE from Pets WHERE Pet_ID = " + petId, connection);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
+            ScriptManager.RegisterStartupScript(this, GetType(), "script", "alert('Record successfully deleted!')", true);
+
+            //resets all inputs (textboxes, buttons, grid)
+            ResetInputs();
+
+            //refresh the data table/grid
+            LoadRecord();
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            ResetInputs();
+        }
+
+        protected void gridviewPets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //assigns the selected row values from the data table (grid) to the textboxes
+            txtPetID.Text = gridviewPets.SelectedRow.Cells[1].Text;
+            txtPetName.Text = gridviewPets.SelectedRow.Cells[2].Text;
+            txtPetAge.Text = gridviewPets.SelectedRow.Cells[3].Text;
+            dropdownSex.Text = gridviewPets.SelectedRow.Cells[4].Text;
+            txtPetBreed.Text = gridviewPets.SelectedRow.Cells[5].Text;
+            txtPetStatus.Text = gridviewPets.SelectedRow.Cells[6].Text;
+            txtPetPrice.Text = gridviewPets.SelectedRow.Cells[7].Text;
+
+            //disables add button, enables update and delete buttons
+            btnAdd.Enabled = false;
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+        }
+    }
+}
